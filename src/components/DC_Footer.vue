ab0e97720ae5a5975e9226cbd2cb1a0c72758a46
antoniostassi <stassiant@outlook.it>
2024-09-10 16:35:47 +0200

    Footer social list

--- src/components/DC_Footer.vue
+++ src/components/DC_Footer.vue
@@ -1,20 +1,43 @@
 <script >
-
+    import DC_Main from './DC_Main.vue';
     export default {
         data(){
             return {
-
+                socialList:[
+                    {
+                        socialLink:"https://facebook.com",
+                        socialIcon:"footer-facebook.png"
+                    },
+                    {
+                        socialLink:"https://x.com",
+                        socialIcon:"footer-twitter.png"
+                    },
+                    {
+                        socialLink:"https://youtube.com",
+                        socialIcon:"footer-youtube.png"
+                    },
+                    {
+                        socialLink:"https://pinterest.com",
+                        socialIcon:"footer-pinterest.png"
+                    },
+                    {
+                        socialLink:"https://periscope.com",
+                        socialIcon:"footer-periscope.png"
+                    }
+                ]
             }
         },
         methods:{
-
+            getImagePath(imgSource){
+                return new URL(`../assets/img/${imgSource}`, import.meta.url).href;
+            }
         }
     }
 
 </script>
 
 <template>
-    <footer class="d-flex flex-column">
+    <footer>
         <div class="dc">
             <div class="container-xxl">
                 <div class="row">
@@ -25,9 +48,19 @@
                 </div>
             </div>
         </div>
-        <div class="lower-footer m-0 py-5 flex-grow-1">
+        <div class="lower-footer m-0 py-5">
             <div class="container-xxl">
-                <button type="button" class="btn btn-outline-primary text-white p-3">SIGN-UP NOW!</button>
+                <div class="row">
+                    <div class="col-6">
+                        <button type="button" class="btn btn-outline-primary text-white p-3">SIGN-UP NOW!</button>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center">
+                        <h3 class="me-3">FOLLOW US</h3>
+                        <ul class="d-flex align-items-center">
+                            <li v-for="(v, index) in socialList" :key="index" class="mx-3"><a :href="v.socialLink"><img :src="getImagePath(v.socialIcon)" alt=""></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
         </div>
