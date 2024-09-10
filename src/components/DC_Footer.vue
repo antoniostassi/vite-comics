84afebdd23bdf22fcbe07f76518708722414668e
antoniostassi <stassiant@outlook.it>
2024-09-10 16:12:47 +0200

    Footer section

--- src/components/DC_Footer.vue
+++ src/components/DC_Footer.vue
@@ -14,11 +14,28 @@
 </script>
 
 <template>
+    <footer class="d-flex flex-column">
+        <div class="dc">
+            <div class="container-xxl">
+                <div class="row">
+                    <div class="col-7"></div>
+                    <div class="col-5">
+                        <img src="../assets/img/dc-logo-bg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lower-footer m-0 py-5 flex-grow-1">
+            <div class="container-xxl">
+                <button type="button" class="btn btn-outline-primary text-white p-3">SIGN-UP NOW!</button>
+            </div>
 
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
 
-    
+    @import url('../assets/scss/partials/components/DC_Footer.scss');
 
 </style>
